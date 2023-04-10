2e895d736901aa3b27c04019f08f5fcc418fbf4c
test01 <test01@user.com>
2023-04-10 14:21:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            double x = double.Parse(s);
+            double F = (Math.Sin(x) - Math.Cos(x)) / 5;
+            Console.WriteLine(x +  +F);
+            Console.ReadKey();
+            return;
+        }
+    }
+}
